7721f46c5af799a75e39dba43a39154c835865f2
mayurim20 <80114491+mayurim20@users.noreply.github.com>
2021-03-14 09:24:42 +0530

    co5-q2

---
+++ DBConnection (3).php
@@ -0,0 +1,10 @@
+//DBConnection(3).php
+
+<?php
+ //Establishing connection with the database
+ define('DB_SERVER', 'localhost');
+ define('DB_USERNAME', 'root');
+ define('DB_PASSWORD', '');
+ define('DB_DATABASE', 'books'); //where profile is the database 
+ $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
